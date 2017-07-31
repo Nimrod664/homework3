f918f4f509c23b4a499b058a9b99d3cd29121186
Mike <mcurrier@qmail.qcc.edu>
2017-07-31 19:07:59 +0000

    edited movie.rb to define all_ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    attr_accessible :title, :rating, :description, :release_date
+    def self.all_ratings
+        arr = Array.new
+        self.select("rating").uniq.each {|x| arr.push(x.rating)}
+        arr.sort.uniq
+    end
 end
