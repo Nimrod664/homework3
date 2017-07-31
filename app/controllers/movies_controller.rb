7b5fcd9defa6faaf3c3543d136defeb4a43b2fe0
Mike <mcurrier@qmail.qcc.edu>
2017-07-31 18:19:06 +0000

    try again

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort_by])
     @sort_column = params[:sort_by]
   end
 
