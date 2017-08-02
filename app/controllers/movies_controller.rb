533a3167a7060aa605450e3abeaaad73244681d8
Mike <mcurrier@qmail.qcc.edu>
2017-08-02 21:58:49 +0000

    edited index in movies_controller for better order

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,31 +12,50 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    @sort_by = params[:sort_by]
-    @ratings = params[:ratings]
     
+    redirect = false
+    logger.debug(session.inspect)
+    
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
+    end
+    
+    if params[:commit] == "Refresh" and params[:rating].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+    
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
+    end
+      
     if @ratings and @sort_by
-      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+      @movies = Movie.where(:rating => @ratings.keys).order(@sort_by)
     elsif @ratings
-      @movies = Movie.where(:rating => params[:ratings].keys)
+      @movies = Movie.where(:rating => @ratings.keys)
     elsif @sort_by
-      @movies = Movie.order(params[sort_by])
+      @movies = Movie.order(@sort_by)
     else
       @movies = Movie.all
     end
     
     if !@ratings
       @ratings = Hash.new
-    end
-    @movies = Movie.all
-    if params[:ratings]
-      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
-    end
-    @sort_column = params[:sort_by]
-    @all_ratings = Movie.all_ratings
-    @set_ratings = params[:set_ratings]
-    if !@set_ratings
-      @set_ratings = Hash.new
     end
   end
 
